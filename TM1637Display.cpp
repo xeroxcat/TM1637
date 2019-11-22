2b4b3bca1f5c9d6d4049b37bf9a7294af2293919
Calvin Houser <cphouser@gmail.com>
2019-11-21 23:22:52 -0800

    start changes

--- TM1637Display.cpp
+++ TM1637Display.cpp
@@ -15,6 +15,8 @@
 //  License along with this library; if not, write to the Free Software
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+
+// modified for use with Mozzi Sound Library 1.0.2
 extern "C" {
   #include <stdlib.h>
   #include <string.h>
